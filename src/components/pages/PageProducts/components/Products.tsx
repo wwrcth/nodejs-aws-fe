2d0bd810da7a1193ba39e0facbcc2c1581a200e2
Katsiaryna Nestserava <Katsiaryna_Nestserava@epam.com>
2021-04-06 16:19:23 +0300

    task3: update products url with own endpoint

--- src/components/pages/PageProducts/components/Products.tsx
+++ src/components/pages/PageProducts/components/Products.tsx
@@ -1,4 +1,5 @@
 import React, {useEffect, useState} from 'react';
+import axios from 'axios';
 import Card from '@material-ui/core/Card';
 import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
@@ -6,12 +7,11 @@ import CardMedia from '@material-ui/core/CardMedia';
 import Grid from '@material-ui/core/Grid';
 import Typography from '@material-ui/core/Typography';
 import {makeStyles} from '@material-ui/core/styles';
+
 import {Product} from "models/Product";
 import {formatAsPrice} from "utils/utils";
 import AddProductToCart from "components/AddProductToCart/AddProductToCart";
-// import axios from 'axios';
-// import API_PATHS from "constants/apiPaths";
-import productList from "./productList.json";
+import API_PATHS from "constants/apiPaths";
 
 const useStyles = makeStyles((theme) => ({
   card: {
@@ -36,9 +36,10 @@ export default function Products() {
   const [products, setProducts] = useState<Product[]>([]);
 
   useEffect(() => {
-    // axios.get(`${API_PATHS.bff}/product/available/`)
-    //   .then(res => setProducts(res.data));
-    setProducts(productList);
+    axios
+      .get(`${API_PATHS.bff}/products/`)
+      .then(res => setProducts(res.data))
+      .catch(err => console.error(err));
   }, [])
 
   return (
@@ -48,7 +49,7 @@ export default function Products() {
           <Card className={classes.card}>
             <CardMedia
               className={classes.cardMedia}
-              image={`https://source.unsplash.com/random?sig=${index}`}
+              image={`https://source.unsplash.com/random?sig=${index}&plants`}
               title="Image title"
             />
             <CardContent className={classes.cardContent}>
