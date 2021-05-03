337d65cb9e701648c53810df95f58db20e73700f
Katsiaryna Nestserava <Katsiaryna_Nestserava@epam.com>
2021-05-03 18:13:03 +0300

    task 5: update import api path

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -2,7 +2,7 @@
 const API_PATHS = {
   product: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
   order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
-  import: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
+  import: 'https://dn8yy07p3a.execute-api.eu-west-1.amazonaws.com/dev',
   bff: 'https://f0203mzoud.execute-api.eu-west-1.amazonaws.com/dev',
   cart: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
 };
